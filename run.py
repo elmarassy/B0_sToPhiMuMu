Fixed constraint for K1s, fixed normalization for plotting, finalized plots

run.py
@@ -25,7 +25,7 @@ coshIntegral, sinhIntegral, cosIntegral, sinIntegral = tools.getIntegrals(accept
 
 
 @jit
-def individualTimeDependent(normalization, sign, cosThetaL, cosThetaK, phi, t, mass,
+def individualTimeDependent(normalization, sign, cosThetaK, cosThetaL, phi, t, mass,
                             K1s, K1c, K2s, K2c, K3, K4, K5, K6s, K7, K8, K9,
                             W1s, W1c, W2s, W2c, W3, W4, W5, W6s, W7, W8, W9,
                             H1s, H1c, H2s, H2c, H3, H4, H5, H6s, H7, H8, H9,
@@ -48,7 +48,7 @@ def individualTimeDependent(normalization, sign, cosThetaL, cosThetaK, phi, t, m
     def helper(coshFactor, cosFactor, H_i, Z_i):
         return coshFactor*(decayPlus + decayMinus)/2. - H_i*(decayPlus - decayMinus)/2. + sign*(cosFactor*jnp.real(osc) - Z_i*jnp.imag(osc))
 
-    return (9.0 / 64.0 / jnp.pi / normalization) * acceptance(t) * (
+    return (9.0 / 64.0 / jnp.pi / normalization) * gamma * (1-y*y) * acceptance(t) * (
             helper(K1s, W1s, H1s, Z1s) * sinThetaK2 +
             helper(K1c, W1c, H1c, Z1c) * cosThetaK2 +
             helper(K2s, W2s, H2s, Z2s) * sinThetaK2 * cos2ThetaL +
@@ -69,7 +69,7 @@ def integrateIndividual(sign,
                         W1s, W1c, W2s, W2c, W3, W4, W5, W6s, W7, W8, W9,
                         H1s, H1c, H2s, H2c, H3, H4, H5, H6s, H7, H8, H9,
                         Z1s, Z1c, Z2s, Z2c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM):
-    return (1./8) * (
+    return (1./8) * (1-y*y) * gamma * (
             (3*W1c + 6*W1s - W2c - 2*W2s) * cosIntegral * sign +
             (3*K1c + 6*K1s - K2c - 2*K2s) * coshIntegral -
             (3*Z1c + 6*Z1s - Z2c - 2*Z2s) * sinIntegral * sign -
@@ -89,16 +89,11 @@ def projectSignalCosThetaL(cosThetaL, sign,
                                W1s, W1c, W2s, W2c, W3, W4, W5, W6s, W7, W8, W9,
                                H1s, H1c, H2s, H2c, H3, H4, H5, H6s, H7, H8, H9,
                                Z1s, Z1c, Z2s, Z2c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM)
-    sin = sinIntegral
-    sinh = sinhIntegral
-    cos = cosIntegral
-    cosh = coshIntegral
-    return (3./(16.*norm)) * (-Z1c*sin - 2*Z1s*sin + Z2c*sin + 2*Z2s*sin - H1c*sinh -
-                              2*H1s*sinh + H2c*sinh + 2*H2s*sinh + 2*cos*K6s*cosThetaL - 2*Z6s*sin*cosThetaL -
-                              2*H6s*sinh*cosThetaL - 2*Z2c*sin*cosThetaL**2 - 4*Z2s*sin*cosThetaL**2 - 2*H2c*sinh*cosThetaL**2 -
-                              4*H2s*sinh*cosThetaL**2 + cos*W1c + 2*cos*W1s - cos*W2c + 2*cos*cosThetaL**2*W2c -
-                              2*cos*W2s + 4*cos*cosThetaL**2*W2s + cosh*(K1c + 2*K1s - K2c - 2*K2s + 2*K2c*cosThetaL**2 + 4*K2s*cosThetaL**2 + 2*cosThetaL*W6s))
 
+    return (3.0/16.0/norm) * gamma * (1 - y*y) * ((W1c + 2*W1s - (1 - 2*cosThetaL**2)*(W2c + 2*W2s) + 2*K6s*cosThetaL)*cosIntegral*sign +
+                                           (K1c + 2*K1s - (1 - 2*cosThetaL**2)*(K2c + 2*K2s) + 2*W6s*cosThetaL)*coshIntegral -
+                                           (Z1c + 2*Z1s - Z2c - 2*Z2s + 2*(Z2c + 2*Z2s)*cosThetaL**2 + 2*Z6s*cosThetaL)*sinIntegral*sign -
+                                           (H1c + 2*H1s - H2c - 2*H2s + 2*(H2c + 2*H2s)*cosThetaL**2 + 2*H6s*cosThetaL)*sinhIntegral)
 
 @jit
 def projectSignalCosThetaK(cosThetaK, sign,
@@ -112,10 +107,20 @@ def projectSignalCosThetaK(cosThetaK, sign,
                                W1s, W1c, W2s, W2c, W3, W4, W5, W6s, W7, W8, W9,
                                H1s, H1c, H2s, H2c, H3, H4, H5, H6s, H7, H8, H9,
                                Z1s, Z1c, Z2s, Z2c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM)
-    return (3./16.) * (
-        ((3*K1c-K2c)*coshIntegral - (3*H1c-H2c)*sinhIntegral + sign*((3*W1c-W2c)*cosIntegral - (3*Z1c-Z2c)*sinIntegral)) * cosThetaK**2 +
-        ((3*K1s-K2s)*coshIntegral - (3*H1s-H2s)*sinhIntegral + sign*((3*W1s-W2s)*cosIntegral - (3*Z1s-Z2s)*sinIntegral)) * (1 - cosThetaK**2)
-    ) / norm
+
+    # f = lambda ctk, values: individualTimeDependent(norm, sign, values[:, 0], ctk, values[:, 1], values[:, 2], values[:, 3],
+    #                                                      K1s, K1c, K2s, K2c, K3, K4, K5, K6s, K7, K8, K9,
+    #                                                      W1s, W1c, W2s, W2c, W3, W4, W5, W6s, W7, W8, W9,
+    #                                                      H1s, H1c, H2s, H2c, H3, H4, H5, H6s, H7, H8, H9,
+    #                                                      Z1s, Z1c, Z2s, Z2c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM)
+    #
+    # return jnp.array([tools.integrator.integrate(lambda values: f(ctk, values), dim=4, N=500000, integration_domain=[cosThetaLRange, phiRange, timeRange, massRange]) for ctk in cosThetaK])
+
+    return -(3.0/16.0/norm)*gamma*(1-y*y)*(
+            (W2s - 3*W1s + (-3*W1c + 3*W1s + W2c - W2s)*cosThetaK**2)*cosIntegral*sign +
+            (K2s - 3*K1s + (-3*K1c + 3*K1s + K2c - K2s)*cosThetaK**2)*coshIntegral -
+            (Z2s - 3*Z1s + (-3*Z1c + 3*Z1s + Z2c - Z2s)*cosThetaK**2)*sinIntegral*sign -
+            (H2s - 3*H1s + (-3*H1c + 3*H1s + H2c - H2s)*cosThetaK**2)*sinhIntegral)
 
 
 @jit
@@ -131,7 +136,7 @@ def projectSignalPhi(phi, sign,
                                H1s, H1c, H2s, H2c, H3, H4, H5, H6s, H7, H8, H9,
                                Z1s, Z1c, Z2s, Z2c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM)
 
-    return (1./(16*jnp.pi*norm)) * (
+    return (1./(16*jnp.pi*norm)) * gamma * (1-y*y) * (
         (3*K1c + 6*K1s - K2c - 2*K2s)*coshIntegral - (3*H1c + 6*H1s - H2c - 2*H2s)*sinhIntegral +
         sign*((3*W1c + 6*W1s - W2c - 2*W2s)*cosIntegral - (3*Z1c + 6*Z1s - Z2c - 2*Z2s)*sinIntegral) +
         4*(K3*coshIntegral - H3*sinhIntegral + sign*(W3*cosIntegral - Z3*sinIntegral))*jnp.cos(2*phi) +
@@ -156,7 +161,7 @@ def projectSignalT(t, sign,
     decayPlus = tools.convolutionIntegral(gamma, y, t)
     decayMinus = tools.convolutionIntegral(gamma, -y, t)
 
-    return (1./8.) * acceptance(t) * (
+    return (1./8.) * (1-y*y) * gamma * acceptance(t) * (
             (3*K1c + 6*K1s - K2c - 2*K2s) * (decayPlus + decayMinus)/2 -
             (3*H1c + 6*H1s - H2c - 2*H2s) * (decayPlus - decayMinus)/2 +
             sign * (
@@ -203,20 +208,18 @@ def integralBackground(c0, c1, c2, kM):
     return tools.integrator.integrate(lambda var: (acceptance(var[:, 0]) * (c0*tools.exponentialDistribution(var[:, 0], c1, timeRange) + (1-c0)*tools.exponentialDistribution(var[:, 0], c2, timeRange))), dim=1, N=9999, integration_domain=[timeRange])
 
 @jit
-def likelihood(signalParams, backgroundParams, f, cosThetaL, cosThetaK, phi, time, mass, qSS, qOS, wSS, wOS, normB, normBBar):
+def likelihood(signalParams, backgroundParams, f, cosThetaK, cosThetaL, phi, time, mass, qSS, qOS, wSS, wOS, normB, normBBar):
     background = backgroundDistribution(time, mass, *backgroundParams) / integralBackground(*backgroundParams)
     weightB = (1 + qSS * (1 - 2 * wSS)) * (1 + qOS * (1 - 2 * wOS)) / 4
     weightBBar = (1 - qSS * (1 - 2 * wSS)) * (1 - qOS * (1 - 2 * wOS)) / 4
-    b = individualTimeDependent(1, 1, cosThetaL, cosThetaK, phi, time, mass, *signalParams)
-    bBar = individualTimeDependent(1, -1, cosThetaL, cosThetaK, phi, time, mass, *signalParams)
+    b = individualTimeDependent(normB, 1, cosThetaL, cosThetaK, phi, time, mass, *signalParams)
+    bBar = individualTimeDependent(normBBar, -1, cosThetaL, cosThetaK, phi, time, mass, *signalParams)
     pdf = (
              weightB *
-             (f*b/normB + (1-f)*background) +
+             (f*b + (1-f)*background) +
              weightBBar *
-             (f*bBar/normBBar + (1-f)*background))
-    weightUntagged = 1 - weightB - weightBBar
-    pdf2 = weightUntagged * (f*(b+bBar)/(normB+normBBar) + (1-f)*background)
-    return -jnp.sum(jnp.log(pdf))#-jnp.sum(jnp.log(pdf2))
+             (f*bBar + (1-f)*background))
+    return -jnp.sum(jnp.log(pdf))
 
 
 @jit
@@ -225,8 +228,12 @@ def helper2(K1c, K3, K4, K5, K6s, K7, K8, K9,
             H1s, H1c, H3, H4, H5, H6s, H7, H8, H9,
             Z1s, Z1c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM):
 
-    K1s = (3.0/4.0) * (1 - y*y - K1c + y*H1c) + y*H1s
-    K2s = (1.0/4.0) * (1 - y*y - K1c + y*H1c) + y*H1s/3.0
+    # K1s = (3.0/4.0) * (1 - y*y - K1c + y*H1c) + y*H1s
+    # K2s = (1.0/4.0) * (1 - y*y - K1c + y*H1c) + y*H1s/3.0
+
+    K1s = (3.0/4.0)*(1 - K1c + y*(4.0*H1s/3.0 + H1c))
+    K2s = K1s / 3.0
+
 
     W2s = W1s/3.0
     H2s = H1s/3.0
@@ -236,6 +243,8 @@ def helper2(K1c, K3, K4, K5, K6s, K7, K8, K9,
     W2c = -W1c
     H2c = -H1c
     Z2c = -Z1c
+
+
 
     normB = integrateIndividual(1,
                                 K1s, K1c, K2s, K2c, K3, K4, K5, K6s, K7, K8, K9,
@@ -262,17 +271,26 @@ def masslessNLLHelper(K1c, K3, K4, K5, K6s, K7, K8, K9,
                                    W1s, W1c, W3, W4, W5, W6s, W7, W8, W9,
                                    H1s, H1c, H3, H4, H5, H6s, H7, H8, H9,
                                    Z1s, Z1c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM)
-    return lambda cosThetaL, cosThetaK, phi, t, mass, qSS, qOS, wSS, wOS: (
-        likelihood(arr, (c0, c1, c2, kM), f, cosThetaL, cosThetaK, phi, t, mass, qSS, qOS, wSS, wOS, normB, normBBar))
+    return lambda cosThetaK, cosThetaL, phi, t, mass, qSS, qOS, wSS, wOS: (
+        likelihood(arr, (c0, c1, c2, kM), f, cosThetaK, cosThetaL, phi, t, mass, qSS, qOS, wSS, wOS, normB, normBBar))
+
 
 @jit
-def value(params, cosThetaL, cosThetaK, phi, time, mass, qSS, qOS, wSS, wOS):
-    return masslessNLLHelper(*params)(cosThetaL, cosThetaK, phi, time, mass, qSS, qOS, wSS, wOS)
+def value(params, cosThetaK, cosThetaL, phi, time, mass, qSS, qOS, wSS, wOS):
+    return masslessNLLHelper(*params)(cosThetaK, cosThetaL, phi, time, mass, qSS, qOS, wSS, wOS)
 
 gradHelper = jax.grad(value, argnums=0)
+
+
+
+
+
+
+
+
 class MasslessNLL:
 
-    def __init__(self, cosThetaL, cosThetaK, phi, time, mass, qSS, qOS, wSS, wOS):
+    def __init__(self, cosThetaK, cosThetaL, phi, time, mass, qSS, qOS, wSS, wOS):
         self.cosThetaL = cosThetaL
         self.cosThetaK = cosThetaK
         self.phi = phi
@@ -295,14 +313,14 @@ class MasslessNLL:
                                        W1s, W1c, W3, W4, W5, W6s, W7, W8, W9,
                                        H1s, H1c, H3, H4, H5, H6s, H7, H8, H9,
                                        Z1s, Z1c, Z3, Z4, Z5, Z6s, Z7, Z8, Z9, m, sigmaM,
-                                       c0, c1, c2, kM, f)(self.cosThetaL, self.cosThetaK, self.phi, self.time, self.mass, self.qSS, self.qOS, self.wSS, self.wOS)
+                                       c0, c1, c2, kM, f)(self.cosThetaK, self.cosThetaL, self.phi, self.time, self.mass, self.qSS, self.qOS, self.wSS, self.wOS)
         if jnp.isfinite(likelihood):
             return float(likelihood)
         return 1e12
 
     @property
     def grad(self):
-        return lambda *params: self.gradientHelper(params, self.cosThetaL, self.cosThetaK, self.phi, self.time, self.mass, self.qSS, self.qOS, self.wSS, self.wOS)
+        return lambda *params: self.gradientHelper(params, self.cosThetaK, self.cosThetaL, self.phi, self.time, self.mass, self.qSS, self.qOS, self.wSS, self.wOS)
 
 @functools.partial(jit, static_argnums=(1,))
 def generate(key, N, signalParams, backgroundParams, f, B0proportion, effSS, effOS, mistagSS, mistagOS):
@@ -325,18 +343,19 @@ def generate(key, N, signalParams, backgroundParams, f, B0proportion, effSS, eff
     cosThetaKSignal = random.uniform(keys[7], shape=(mul*N,), minval=-1.0, maxval=1.0)
     phiSignal = random.uniform(keys[8], shape=(mul*N,), minval=-jnp.pi, maxval=jnp.pi)
     tSignal = random.uniform(keys[9], shape=(mul*N,), minval=timeRange[0], maxval=timeRange[1])
+    # massSignal = random.normal(keys[10], shape=(mul*N,)) * 5*signalParams[-1] + signalParams[-2]
     massSignal = random.uniform(keys[10], shape=(mul*N,), minval=massRange[0], maxval=massRange[1])
     normB = integrateIndividual(1, *signalParams)
     normBBar = integrateIndividual(-1, *signalParams)
     norm = jnp.where(trueTag, normB, normBBar)
-    signalProbabilities = individualTimeDependent(norm, trueTagValue, cosThetaLSignal, cosThetaKSignal, phiSignal, tSignal, massSignal, *signalParams)
+    signalProbabilities = individualTimeDependent(norm, trueTagValue, cosThetaKSignal, cosThetaLSignal, phiSignal, tSignal, massSignal, *signalParams)
     signalProbabilities /= signalProbabilities.max()
 
     cosThetaLBackground = random.uniform(keys[11], shape=(mul*N,), minval=-1.0, maxval=1.0)
     cosThetaKBackground = random.uniform(keys[12], shape=(mul*N,), minval=-1.0, maxval=1.0)
     phiBackground = random.uniform(keys[13], shape=(mul*N,), minval=-jnp.pi, maxval=jnp.pi)
     tBackground = random.uniform(keys[14], shape=(mul*N,), minval=timeRange[0], maxval=timeRange[1])
-    massBackground = random.uniform(keys[15], shape=(mul*N,), minval = massRange[0], maxval=massRange[1])
+    massBackground = random.uniform(keys[15], shape=(mul*N,), minval=massRange[0], maxval=massRange[1])
     backgroundProbabilities = backgroundDistribution(tBackground, massBackground, *backgroundParams)
     backgroundProbabilities /= backgroundProbabilities.max()
 
@@ -368,17 +387,16 @@ def generate(key, N, signalParams, backgroundParams, f, B0proportion, effSS, eff
     tagChoiceSS = jnp.where(signal, tagChoiceSSSignal, tagChoiceSSBackground)
     tagChoiceOS = jnp.where(signal, tagChoiceOSSignal, tagChoiceOSBackground)
 
-    return cosThetaL, cosThetaK, phi, t, mass, tagChoiceSS, tagChoiceOS, jnp.full(N, mistagSS), jnp.full(N, mistagOS)
+    return cosThetaK, cosThetaL, phi, t, mass, tagChoiceSS, tagChoiceOS, jnp.full(N, mistagSS), jnp.full(N, mistagOS)
 
 
-def run(nEvents, nToys, effSS, effOS, wSS, wOS, saveProjectionValue=4, massless=True, B0proportion=0.5, untagged=False):
+def run(nEvents, nToys, effSS, effOS, wSS, wOS, tagged, q2_range, generationName, B0proportion=0.5):
     key = jax.random.key(0)
-
-    if untagged:
+    if not tagged:
         effSS = 0
         effOS = 0
-    paramNames = generation.getFitParamNames(massless)
-    pars = generation.getFitParams(massless)
+    paramNames = generation.getFitParamNames(True)
+    pars = generation.getFitParams(True, q2_range, generationName)
     trueValues = np.array(pars[0] + pars[1] + [pars[2]])
     savedData = []
     values = np.empty(shape=(nToys, len(paramNames)))
@@ -390,7 +408,7 @@ def run(nEvents, nToys, effSS, effOS, wSS, wOS, saveProjectionValue=4, massless=
     while i < nToys:
         key, key1 = jax.random.split(key)
         tStart = time.time()
-        pars = generation.getFitParams(massless)
+        pars = generation.getFitParams(True, q2_range, generationName)
         data = generate(key1, nEvents, generation.getAllSignalParamsFromMassless(*pars[0]), pars[1], pars[2], B0proportion, effSS, effOS, wSS, wOS)
         nll = MasslessNLL(*data)
         m = Minuit(nll, *pars[0], *pars[1], pars[2])
@@ -398,7 +416,7 @@ def run(nEvents, nToys, effSS, effOS, wSS, wOS, saveProjectionValue=4, massless=
         limits = [(-1 - 4*abs(param), 1 + 4*abs(param)) for param in trueValues]
         limits[generation.getFitParamNames(True).index("$f$")] = (0, 1)
         limits[generation.getFitParamNames(True).index("$c_0$")] = (0, 1)
-        if untagged:
+        if not tagged:
             m.fixed['W1s'] = True
             m.fixed['W1c'] = True
             m.fixed['W3'] = True
@@ -417,16 +435,17 @@ def run(nEvents, nToys, effSS, effOS, wSS, wOS, saveProjectionValue=4, massless=
             m.fixed['Z7'] = True
             m.fixed['Z8'] = True
             m.fixed['Z9'] = True
+
         m.limits = limits
         m.strategy = 0
         m.errordef = Minuit.LIKELIHOOD
         m.migrad()
         m.hesse()
-        print('number: ', m.nfcn)
+        # print('number: ', m.nfcn)
         if not m.fmin.is_valid or not m.fmin.has_accurate_covar or m.fmin.has_parameters_at_limit or m.fmin.hesse_failed:
-            print(m.fmin)
-            print(m.params)
-            print(f"Failed fit #{i} in {time.time() - tStart:.3f} seconds")
+            # print(m.fmin)
+            # print(m.params)
+            # print(f"Failed fit #{i} in {time.time() - tStart:.3f} seconds")
             failedCounter += 1
             continue
         vals = np.array(m.values)
@@ -434,39 +453,30 @@ def run(nEvents, nToys, effSS, effOS, wSS, wOS, saveProjectionValue=4, massless=
         values[i, :] = vals
         errors[i, :] = errs
         pulls[i, :] = (vals - trueValues) / errs
-        if np.any(np.abs(pulls[i, :]) > saveProjectionValue):
-            savedData.append((data, m.covariance.correlation()))
+        if i < 10:
+            savedData.append(data)
         print(f"Performed fit #{i} in {time.time() - tStart:.3f} seconds")
         i += 1
     if failedCounter > 0:
         print(f"Failed {failedCounter}/{nToys} fits")
     return paramNames, pulls, values, savedData
 
-def main():
-
-    nEvents = 6000
-    nToys = 3
-    wSS = generation.trueValues['wSS']
-    wOS = generation.trueValues['wOS']
-    effSS = generation.trueValues['effSS']
-    effOS = generation.trueValues['effOS']
-
-    names, pulls, values, savedData = run(nEvents, nToys, effSS, effOS, wSS, wOS, untagged=False)
-    plot.plot(names, pulls, values, savedData, nEvents, nToys, 'plots')
-
-
-    # plot.project(saveProjectionsValue, [], [], [], effSS, effOS, wSS, wOS, "plots")
-    #
-    # plot.plotProjectionSummary(saveProjectionsValue, pulls, values, savedData, effSS, effOS, wSS, wOS, "plots")
-    # plot.plotSummary(names, pulls, True, False, "plots")
-    # plot.plotSummary(names, values, True, True, "plots", generation.getFitParams(massless))
-
 
 if __name__ == "__main__":
-    # profiler = cProfile.Profile()
-    # profiler.enable()
-    import plot
-    main()
-    # profiler.disable()
-    # stats = pstats.Stats(profiler).sort_stats('time')
-    # stats.print_stats(100)
+
+    from plot import plot
+    nToys = 1000
+    B0proportion = 0.5
+    for generationName in ['theory', 'alternative']:
+        for q2_range in range(3):
+            for luminosity in range(3):
+                nEvents = int(generation.q2_yields[luminosity][q2_range] / generation.trueValues["$f$"])
+                for tagging in ['untagged', 'badTagging', 'goodTagging']:
+                    directory = f'{tagging}/{generationName}_{generation.q2_range_names[q2_range]}_{generation.integratedLuminosities[luminosity]}'
+                    wSS = generation.tagging[tagging]['wSS']
+                    wOS = generation.tagging[tagging]['wOS']
+                    effSS = generation.tagging[tagging]['effSS']
+                    effOS = generation.tagging[tagging]['effOS']
+                    names, pulls, values, savedData = run(nEvents, nToys, effSS, effOS, wSS, wOS, tagging != 'untagged', q2_range, generationName, B0proportion=B0proportion)
+                    plot(names, pulls, values, savedData, nEvents, nToys, directory, q2_range, luminosity, generationName, tagging, B0proportion=B0proportion)
+                    print(f'Completed {directory}')

tools.py
@@ -5,7 +5,7 @@ from iminuit import Minuit
 from jax import jit, vmap
 import jax.numpy as jnp
 from torchquad import Simpson, set_up_backend
-
+from scipy.stats import chi2
 set_up_backend("jax", data_type="float64")
 integrator = Simpson()
 
@@ -103,11 +103,23 @@ def convolutionIntegral(gamma, z, t):
 
 def getIntegrals(acceptance, timeRange, gamma, x, y):
     coshIntegral = integrator.integrate(lambda time: acceptance(time) * ((convolutionIntegral(gamma, y, time) + convolutionIntegral(gamma, -y, time))/2.0),
-                                        dim=1, N=99999, integration_domain=[timeRange])
+                                        dim=1, N=999999, integration_domain=[timeRange])
     sinhIntegral = integrator.integrate(lambda time: acceptance(time) * ((convolutionIntegral(gamma, y, time) - convolutionIntegral(gamma, -y, time))/2.0),
-                                        dim=1, N=99999, integration_domain=[timeRange])
+                                        dim=1, N=999999, integration_domain=[timeRange])
     cosIntegral = integrator.integrate(lambda time: acceptance(time) * jnp.real(convolutionIntegral(gamma, 1j*x, time)),
-                                       dim=1, N=99999, integration_domain=[timeRange])
+                                       dim=1, N=999999, integration_domain=[timeRange])
     sinIntegral = integrator.integrate(lambda time: acceptance(time) * jnp.imag(convolutionIntegral(gamma, 1j*x, time)),
-                                       dim=1, N=99999, integration_domain=[timeRange])
+                                       dim=1, N=999999, integration_domain=[timeRange])
     return coshIntegral, sinhIntegral, cosIntegral, sinIntegral
+
+def poissonError(n):
+    alpha = 1 - 0.6827
+    if n == 0:
+        lower = 0
+        upper = 0.5 * chi2.ppf(1 - alpha / 2, 2 * (n + 1))
+    else:
+        lower = 0.5 * chi2.ppf(alpha / 2, 2 * n)
+        upper = 0.5 * chi2.ppf(1 - alpha / 2, 2 * (n + 1))
+    err_lower = n - lower
+    err_upper = upper - n
+    return err_lower, err_upper